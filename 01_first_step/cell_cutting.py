97178ee8e1f6e28e7e1ac10cc2769e41643d7df0
Kirill Eliseev <eliseevkirill@gmail.com>
2023-01-14 14:29:38 +0400

    added check for out of pixel range

--- 01_first_step/cell_cutting.py
+++ 01_first_step/cell_cutting.py
@@ -3,19 +3,13 @@
 # + find cell size
 # + crop cells
 # + add cells to list
+# + boundary conditions check
 # the file should be in the same folder as the function
 
 import cv2
 
 
-def get_cutting_pictures(row, column, file_name):
-    # read image, file_name as string
-    image = cv2.imread(file_name)
-
-    # find height and width
-    width = image.shape[0]
-    height = image.shape[1]
-
+def get_crop(image, width, height, column, row):
     # find cell size
     cell_height = height // column
     cell_width = width // row
@@ -33,3 +27,18 @@ def get_cutting_pictures(row, column, file_name):
             cropped_pictures.append(cropped)
         x += cell_height
     return cropped_pictures
+
+
+def get_cropping_pictures(row, column, file_name):
+    # read image, file_name as string
+    image = cv2.imread(file_name)
+
+    # find height and width
+    width = image.shape[0]
+    height = image.shape[1]
+
+    # boundary conditions check
+    if row > width or 0 >= row or column > height or column <= 0:
+        print("outside of pixel sizes or pixel range")
+    else:
+        return get_crop(image, width, height, column, row)
