a2e0ba6e1e85ab087ce534679e53bdfccb3016b5
Kirill Eliseev <eliseevkirill@gmail.com>
2023-01-16 19:37:12 +0400

    - changed: changed method of cutting pictures - added: gap optional parameter - added: adding incomplete cells - test: gap boundary conditions

--- 01_first_step/cell_cutting.py
+++ 01_first_step/cell_cutting.py
@@ -5,7 +5,7 @@
 # + add cells to list
 # + boundary conditions check
 # + add border
-#   add gap
+# + add gap
 # the file should be in the same folder as the function
 
 import cv2
@@ -16,36 +16,54 @@ def crop_image(file_name, rows=1, columns=1, border=0, gap=0):
     image = cv2.imread(file_name)
 
     # find height and width
-    width = image.shape[0]
-    height = image.shape[1]
+    height = image.shape[0]
+    width = image.shape[1]
 
     # boundary conditions check
     if (rows > width or rows <= 0) or (columns > height or columns <= 0):
         print("outside of pixel sizes or pixel range")
     elif border < 0 or border >= width // 2 or border >= height // 2:
         print("border out of range")
+    elif gap < 0 or gap > (height - gap * rows) // 2 or gap > (width - gap * columns) // 2:
+        print("gap out of range")
     else:
         # border
-        bordered_image = image[0 + border:width - border, 0 + border:height - border]
+        bordered_image = image[0 + border:height - border, 0 + border:width - border]
 
         # find height and width
-        width = bordered_image.shape[0]
-        height = bordered_image.shape[1]
+        height = bordered_image.shape[0]
+        width = bordered_image.shape[1]
 
         # find cell size
-        cell_width = width // rows
-        cell_height = height // columns
+        cell_height = (height - gap * rows) // rows
+        cell_width = (width - gap * columns) // columns
 
         # crop cells
-        x = 0
         cropped_pictures = []
-        while x + cell_height <= height:
-            y = 0
-            while y + cell_width <= width:
-                cropped = bordered_image[y:y + cell_width, x:x + cell_height]
-                y += cell_width
 
-                # add cells to list
+        x = 0
+        for count_rows in range(columns):
+            y = 0
+            for count_columns in range(rows):
+                cropped = bordered_image[y:y + cell_height, x:x + cell_width]
                 cropped_pictures.append(cropped)
-            x += cell_height
+                y = y + cell_height + gap
+            x = x + cell_width + gap
+
+        # adding incomplete cells
+        if x < width:
+            y = 0
+            for count_columns in range(rows):
+                print(x, ":", y)
+                cropped = bordered_image[y:y + cell_height, x:width]
+                cropped_pictures.append(cropped)
+                y = y + cell_height + gap
+        if y < height:
+            x = 0
+            for count_rows in range(columns):
+                print(x, ":", y)
+                cropped = bordered_image[y:height, x:x + cell_width]
+                cropped_pictures.append(cropped)
+                x = x + cell_height + gap
+
         return cropped_pictures
